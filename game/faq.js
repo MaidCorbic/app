389e05e33588eb5c22f99c286e0a41258842b3af
Maid Čorbić <detrix233@gmail.com>
2026-08-14 14:11:16 +0200

    Add FAQ and latest update content

---
+++ game/faq.js
@@ -0,0 +1,24 @@
+export const RELAY_FAQ = [
+  ['Kako se igra?', 'Koristi A/D ili lijevu/desnu stranu joysticka za kretanje, SPACE ili JUMP za skok. Na telefonu koristi touch kontrole.'],
+  ['Kako završavam misiju?', 'Prati cilj misije, sakupi potrebne signale i stigni do delivery beacona. Nakon završetka koristi NEXT MISSION za nastavak.'],
+  ['Kako prelazim na sljedeću misiju?', 'Nakon uspješno završene misije pritisni NEXT MISSION. Igra automatski učitava naredni level.'],
+  ['Šta rade SWORD, DASH i BUILD?', 'SWORD služi za borbu, DASH za brzo izbjegavanje prepreka, a BUILD opcije aktiviraju dostupne građevinske sposobnosti.'],
+  ['Mogu li igrati na telefonu?', 'Da. Igra ima touch kontrole i preporučuje landscape način rada za najbolji pregled.'],
+  ['Zašto nema zvuka?', 'Mobilni browseri mogu blokirati automatski zvuk. Prvi dodir ekrana omogućava muziku; provjeri i mute/postavke zvuka uređaja.'],
+  ['Da li se napredak čuva?', 'Napredak misija i dostupni podaci koriste postojeći sistem igre. Ne briši podatke browsera ako želiš zadržati lokalni napredak.'],
+  ['Šta znači XP?', 'XP predstavlja iskustvo koje dobijaš završavanjem misija i utiče na napredak/rang.'],
+  ['Kako pauzirati igru?', 'Pritisni dugme ☰ u gornjem desnom uglu HUD-a.'],
+  ['Gdje vidim najnovije promjene?', 'Pritisni kružni i simbol u gornjem desnom uglu početnog ekrana za otvaranje Latest Update panela.']
+];
+
+export const LATEST_UPDATE = {
+  version: 'LATEST UPDATE',
+  title: 'MISSION SYSTEM STABILITY',
+  items: [
+    'Pouzdaniji prelaz između misija bez crnog ekrana.',
+    'Stabilniji WebGL resize tokom mission transitiona.',
+    'Originalna arcade menu muzika.',
+    'Bolja podrška za touch kontrole na telefonu.',
+    'FAQ i informativni panel dostupni direktno iz menija.'
+  ]
+};
