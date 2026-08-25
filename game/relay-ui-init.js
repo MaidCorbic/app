1405c01ea2d98b57c428a45d363b2b645eee3543
Maid Čorbić <detrix233@gmail.com>
2026-08-25 20:33:49 +0200

    feat: add route mutation
    * feat: add route mutation
    
    * feat: wire route mutation

--- game/relay-ui-init.js
+++ game/relay-ui-init.js
@@ -52,6 +52,7 @@ import './world-interaction-runtime-v2.js';
 import './environmental-force-zone-v1.js';
 import './timed-energy-trap-v1.js';
 import './pressure-route-node-v1.js';
+import './route-mutation-v1.js';
 import './player-shield-visual-cleanup-v1.js';
 import './tutorial-runtime-gate-v1.js';
 import './home-tutorial-v1.js';
