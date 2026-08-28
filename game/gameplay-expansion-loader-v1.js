0ac53d3dd60bf663f441bb92e1d9590b93a73449
Maid Čorbić <detrix233@gmail.com>
2026-08-28 20:13:49 +0200

    Load gameplay expansion v2 through existing loader

--- game/gameplay-expansion-loader-v1.js
+++ game/gameplay-expansion-loader-v1.js
@@ -1,4 +1,6 @@
 import { RunnerScene } from './src/scenes/RunnerScene.js';
 import { installGameplayExpansion } from './src/systems/gameplay-expansion-v1.js';
+import { installGameplayExpansionV2Safe } from './src/systems/gameplay-expansion-v2-safe.js';
 
 installGameplayExpansion(RunnerScene);
+installGameplayExpansionV2Safe(RunnerScene);
