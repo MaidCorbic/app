5e1de8f4558ab2f0869ec82b518ac41a07e678cf
Maid Čorbić <detrix233@gmail.com>
2026-08-29 18:47:28 +0200

    refactor(gameplay): remove legacy fake map injectors

--- game/home-options.js
+++ game/home-options.js
@@ -5,6 +5,4 @@ import './src/systems/audio-autoplay-guard-v1.js';
 import './home-navigation-final-v2.js';
 import './gameplay-feel-v3.js';
 import './gameplay-feature-dock-v1.js';
-import './gameplay-intro-prologue-map-v1.js';
-import './gameplay-intro-tactical-map-v2.js';
 import './gameplay-expansion-loader-v1.js';
