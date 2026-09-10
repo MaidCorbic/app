11e6f8057ca3ec37b4267a149c3bd748d6ee83d5
Maid Čorbić <detrix233@gmail.com>
2026-09-10 10:58:41 +0200

    feat: activate settings home redesign

--- game/relay-ui-init.js
+++ game/relay-ui-init.js
@@ -7,6 +7,7 @@ import './cargo-integrity-v2-polish.css';
 import './signal-network-v1.css';
 import './city-response-v1.css';
 import './gameplay-ui-v8-settings-polish.css';
+import './gameplay-ui-v9-settings-home-redesign.css';
 
 import './gameplay-ui-visibility-v3.js';
 import './gameplay-hud-polish-v1.css';
