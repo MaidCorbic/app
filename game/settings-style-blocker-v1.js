fef1e8e3e5d72585dc1f79ff211f6a08269abd83
Maid Čorbić <detrix233@gmail.com>
2026-09-10 15:56:21 +0200

    fix: block legacy settings inline stylesheet

---
+++ game/settings-style-blocker-v1.js
@@ -0,0 +1,15 @@
+/* Block the retired inline Options stylesheet before it can enter <head>. */
+(() => {
+  const LEGACY_ID = 'relay-unified-options-style';
+  const nativeAppendChild = Node.prototype.appendChild;
+  const nativeInsertBefore = Node.prototype.insertBefore;
+  const isLegacyStyle = node => node?.nodeType === 1 && node.tagName === 'STYLE' && node.id === LEGACY_ID;
+  Node.prototype.appendChild = function(node) {
+    if (isLegacyStyle(node)) return node;
+    return nativeAppendChild.call(this, node);
+  };
+  Node.prototype.insertBefore = function(node, referenceNode) {
+    if (isLegacyStyle(node)) return node;
+    return nativeInsertBefore.call(this, node, referenceNode);
+  };
+})();
