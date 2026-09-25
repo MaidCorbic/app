41420039540b0fb3942c7fb3efc8ec6938263bfd
Maid Čorbić <detrix233@gmail.com>
2026-09-25 12:22:30 +0200

    fix: remove obsolete gameplay briefing modules

--- game/relay-ui-init.js
+++ game/relay-ui-init.js
@@ -123,11 +123,6 @@ import './play-deployment-loader-v1.js';
 
 import './mission-transition-loader-v1.js';
 
-import './gameplay-intro-final-v1.js';
-
-import './mission-route-v6-layout-v1.js';
-import './mission-route-v6-layout-v1.css';
-
 import './mobile-map-web-parity-all-levels-v1.js';
 
 import './gameplay-core-v1.js';
